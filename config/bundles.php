73a6ffb0d9c73f2cda10747b5548bdf057348f84
soo-essoklina-ulrich <essoklina.soo@ipnetinstitute.com>
2024-04-15 17:45:04 +0000

    Add EasyAdminBundle to composer.json and config/bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
