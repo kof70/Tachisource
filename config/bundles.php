582a915d2d33a576d52a09a935a65aefa10fcc93
soo-essoklina-ulrich <essoklina.soo@ipnetinstitute.com>
2024-04-20 18:10:54 +0000

    add esay bundle security mise en place des fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Langue;
+use App\Entity\TypeExtension;
+use App\Entity\Utilisateur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = new Utilisateur();
+
+        $user->setUsername('admin');
+        $user->setPassword('$2y$13$peRq/KiopZfPtGrAc8LsP.0tSCiz7Qhbu8.Q4iyE2wsgwayePboKC');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
+
+        $user1 = new Utilisateur();
+        $user1->setUsername('user');
+        $user1->setPassword('$2y$13$bhZoyHUotN2hyxfzLlsN1eq5sBw21dbya.FYdmLoFOQeTRiv/8l4K');
+        $user1->setRoles(['ROLE_USER']);
+
+        $manager->persist($user1);
+
+        $langue = new Langue();
+        $langue->setSigle('Fr');
+        $langue->setNomlangue('Français');
+
+        $manager->persist($langue);
+
+        $langue1 = new Langue();
+        $langue1->setSigle('En');
+        $langue1->setNomlangue('Anglais');
+
+        $manager->persist($langue1);
+
+        $type = new TypeExtension();
+        $type->setTitre('Hentai');
+
+        $manager->persist($type);
+
+        $type1 = new TypeExtension();
+        $type1->setTitre('Ecchi');
+
+        $manager->persist($type1);
+
+        $type2 = new TypeExtension();
+        $type2->setTitre('Yaoi');
+
+        $manager->persist($type2);
+
+        $manager->flush();
+    }
+}
