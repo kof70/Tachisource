efc972bf03831df7ad0a0cc7265f373e9fc44d6f
soo-essoklina-ulrich <essoklina.soo@ipnetinstitute.com>
2024-04-30 09:57:16 +0000

    extension page for user and custom all page with code divider

--- src/Controller/Admin/ExtensionCrudController.php
+++ src/Controller/Admin/ExtensionCrudController.php
@@ -3,10 +3,20 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Extension;
+use App\Entity\TypeExtension;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\LanguageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Filter\Type\EntityFilterType;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class ExtensionCrudController extends AbstractCrudController
 {
@@ -15,14 +25,23 @@ class ExtensionCrudController extends AbstractCrudController
         return Extension::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IntegerField::new('id', 'ID')->onlyOnIndex(),
+            TextField::new('apkFile', 'Fichier APK')->setFormType(VichFileType::class),
+            CollectionField::new('type')->setFormTypeOptions([
+                'entry_type' => EntityType::class,
+                'entry_options' => [
+                    'class' => TypeExtension::class,
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+            ]),
+            LanguageField::new('langue')->showCode(true),
         ];
     }
-    */
+
 }
